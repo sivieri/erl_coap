007dc8a98c2ba971152696d971e31caace3ff9aa
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2014-03-11 14:49:44 +0100

    Moved testing stuff out of the way; added a README.

---
+++ README.md
@@ -0,0 +1,4 @@
+CoAP (partial) library for Erlang
+==========================
+
+This library implements (as of today) only one of the [CoAP methods](http://tools.ietf.org/html/draft-ietf-core-coap-18), the GET method, using [CantCoAP](https://github.com/staropram/cantcoap) as a serialization library for the C driver.
--- include/eliot.hrl
+++
@@ -1,9 +0,0 @@
--type(net()::{[atom()], dict()} | {[{atom(), atom()}], dict()}).
--define(NOISE_AVG, -75.0).
--define(NOISE_DELTA, 5.0).
--define(SENSITIVITY, 4.0).
--define(NODENAME, "eliot").
--define(INTERFACE, "wlan0").
--define(RPC_NOACKS, 30000).
--type swtype() :: gettimeofday | clock_gettime | clock | times | getrusage.
--record(stopwatch, {type = gettimeofday :: swtype(), start = none, cur = none, startacc = none, acc = none, last = none}).
